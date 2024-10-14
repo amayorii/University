8462fae27c07b08a5b72d2988f05b36477ee0af6
Nazariy <malashenko.naz@gmail.com>
2024-10-14 09:03:50 +0300

    added matrix operator overloads

--- Laba2/MatrixOperations.cs
+++ Laba2/MatrixOperations.cs
@@ -4,5 +4,34 @@ namespace Laba2;
 
 partial class MyMatrix
 {
+    public static MyMatrix operator +(MyMatrix matrix1, MyMatrix matrix2)
+    {
+        if (matrix1.Width != matrix2.Width || matrix1.Height != matrix2.Height) throw new Exception("Matrixes must be the same!");
+        MyMatrix matrix3 = new MyMatrix(new double[matrix1.Height, matrix1.Width]);
+        for (int i = 0; i < matrix1.Height; i++)
+        {
+            for (int j = 0; j < matrix1.Width; j++)
+            {
+                matrix3.SetMatrixElement(i, j, matrix1[i, j] + matrix2[i, j]);
+            }
+        }
+        return matrix3;
+    }
 
+    public static MyMatrix operator *(MyMatrix matrix1, MyMatrix matrix2)
+    {
+        if (matrix1.Width != matrix2.Height) throw new Exception("Matrix A rows must equal matrix B cols");
+        MyMatrix matrix3 = new MyMatrix(new double[matrix1.Height, matrix2.Width]);
+        for (int i = 0; i < matrix1.matrixElems.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix2.matrixElems.GetLength(1); j++)
+            {
+                for (int k = 0; k < matrix2.matrixElems.GetLength(0); k++)
+                {
+                    matrix3[i, j] += matrix1[i, k] * matrix2[k, j];
+                }
+            }
+        }
+        return matrix3;
+    }
 }
