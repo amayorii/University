dbf823b2e8db581647be49a5da9246a11a5f847a
Nazariy <malashenko.naz@gmail.com>
2024-11-10 22:07:48 +0200

    add mirrorBtn action, image display & other small changes

--- Laba3UI/MainWindow.xaml.cs
+++ Laba3UI/MainWindow.xaml.cs
@@ -2,8 +2,9 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Text.RegularExpressions;
+using System.Text;
 using System.Windows;
+using System.Windows.Media.Imaging;
 namespace Laba3UI
 {
     /// <summary>
@@ -14,114 +15,21 @@ namespace Laba3UI
         readonly static string src = new DirectoryInfo(Directory.GetCurrentDirectory()).Parent!.Parent!.Parent!.FullName;
         public MainWindow()
         {
+            Icon = BitmapFrame.Create(new Uri($"{src}\\mirrorIcon.ico", UriKind.RelativeOrAbsolute));
             InitializeComponent();
-            //MessageBox.Show(AvgSum().ToString());
             ClearFiles();
         }
         static void ClearFiles()
         {
-            string[] files = Directory.GetFiles($@"{src}\InvalidData\");
+            string[] files = Directory.GetFiles($@"{src}\MirroredGifs\");
             foreach (var item in files) File.Delete(item);
-            files = Directory.GetFiles($@"{src}\MirroredGifs\");
         }
-        static double AvgSum()
-        {
-            double avg = 0;
-            ushort count = 0;
-            byte number = 1;
-            for (int i = 10; i < 30; i++)
-            {
-                string path = $@"{src}\txts\{i}.txt";
-                try
-                {
-                    var file = new FileStream(path, FileMode.Open);
-                    StreamReader reader = new StreamReader(file);
-                    int num1 = int.Parse(reader.ReadLine()!);
-                    int num2 = int.Parse(reader.ReadLine()!);
-                    file.Close();
-                    reader.Close();
-                    checked
-                    {
-                        num1 *= num2;
-                    }
-                    avg += num1;
-                    count++;
-                }
-                catch (OverflowException)
-                {
-                    System.Console.WriteLine($"Exception {number}\nReason: numbers are too big and their product cannot exceed {int.MaxValue}/{-int.MaxValue}\nFile name: {Path.GetFileName(path)}\n");
-                    AppendLog("overflow", path);
-                    number++;
-                }
-                catch (ArgumentNullException)
-                {
-                    System.Console.WriteLine($"Exception {number}\nReason: file must contain only 1 integer in first and 1 integer in second lines\nFile name: {Path.GetFileName(path)}\n");
-                    AppendLog("bad_data", path);
-                    number++;
-                }
-                catch (FormatException ex)
-                {
-                    System.Console.WriteLine($"Exception {number}\nReason: {ex.Message.ToLower()}\nFile name: {Path.GetFileName(path)}\n");
-                    AppendLog("bad_data", path);
-                    number++;
-                }
-                catch (FileNotFoundException)
-                {
-                    System.Console.WriteLine($"Exception {number}\nReason: file wasn't found\nFile name: {Path.GetFileName(path)}\n");
-                    AppendLog("no_file", path);
-                    number++;
-                }
-                catch (Exception ex)
-                {
-                    System.Console.WriteLine(ex.Message);
-                }
-            }
-            return avg / count;
-        }
-        static void AppendLog(string name, string path)
-        {
-            try
-            {
-                FileStream creator = new FileStream($@"{src}\InvalidData\{name}.txt", FileMode.Append);
-                StreamWriter streamWriter = new StreamWriter(creator);
-                streamWriter.WriteLine(Path.GetFileName(path));
-                streamWriter.Close();
-                creator.Close();
-            }
-            catch (Exception)
-            {
-                System.Console.WriteLine($"Couldn't create or update {name}.txt file");
-            }
-        }
-        static void MirroredGifs(string path)
-        {
-            var images = Directory.GetFiles(path);
-            Regex regexExtForImage = new("^((.bmp)|(.gif)|(.tiff?)|(.jpe?g)|(.png)|(.ico)|(.svg)|(.pdf)|(.avif)|(.webp))$", RegexOptions.IgnoreCase);
-            foreach (var image in images)
-            {
-                if (regexExtForImage.IsMatch(Path.GetExtension(image)))
-                {
-                    try
-                    {
-                        Bitmap img = new(image);
-                        img.RotateFlip(RotateFlipType.Rotate180FlipX);
-                        img.Save($@"{src}\MirroredGifs\{Path.GetFileNameWithoutExtension(image)}-mirrored.gif");
-                    }
-                    catch (Exception ex)
-                    {
-                        System.Console.WriteLine(ex.Message);
-                        System.Console.WriteLine("error in " + Path.GetFileNameWithoutExtension(image));
-                    }
-                }
-            }
-        }
-
         private void selectFileBtn_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fd = new OpenFileDialog();
             fd.Multiselect = true;
             fd.InitialDirectory = @$"{src}\images\";
-            fd.Filter = "Image Files | *.jpg; *.jpeg; *.png; *.tiff; *.tif; *.svg; *.bmp; *.gif; *.ico; *.avif";
+            fd.Filter = "Image Files | *.jpg; *.jpeg; *.png; *.tiff; *.tif; *.svg; *.bmp; *.gif; *.ico; *.avif; *.webp";
             if (fd.ShowDialog() == true)
             {
                 foreach (var image in fd.FileNames)
@@ -137,7 +45,24 @@ namespace Laba3UI
 
         private void mirrorFilesBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string image in PictureList.SelectedItems)
+            {
+                var path = $@"{src}\MirroredGifs\{Path.GetFileNameWithoutExtension(image)}-mirrored.gif";
+                try
+                {
+                    Image img = Image.FromFile(image);
+                    img.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    img.Save(path);
+                    img.Dispose();
+                    if (!this.ReflectedList.Items.Contains(path)) this.ReflectedList.Items.Add(path);
+                }
+                catch (Exception ex)
+                {
+                    stringBuilder.AppendLine(ex.Message + "\nError in " + Path.GetFileNameWithoutExtension(image) + "\n");
+                }
+            }
+            if (stringBuilder.Length > 0) MessageBox.Show(stringBuilder.ToString());
         }
         private void ReflectedList_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
