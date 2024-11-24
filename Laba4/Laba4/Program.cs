6aa11b4416d064a474eae4ba65d7756343c4cbf0
Nazariy <malashenko.naz@gmail.com>
2024-11-24 17:12:24 +0200

    add fourth lab

---
+++ Laba4/Laba4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Laba4;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ Laba4/Laba4UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Laba4UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
