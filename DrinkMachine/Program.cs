731f37da90c9bffb100f3c0eaf33590d17a2552e
Nazariy <malashenko.naz@gmail.com>
2024-11-29 19:08:51 +0200

    add choosing

--- DrinkMachine/Program.cs
+++ DrinkMachine/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace DrinkMachine;
+﻿using Figgle;
+
+namespace DrinkMachine;
 
 class Program
 {
     static void Main()
     {
-
+        DrinkMachine dm = new();
+        System.Console.WriteLine(FiggleFonts.Standard.Render("|  Choose a drink  |"));
+        System.Console.WriteLine(FiggleFonts.Standard.Render("   Coffee Tea Juice"));
+        switch (Console.ReadLine()!.ToLower())
+        {
+            case "coffee":
+                dm.GetCoffee();
+                return;
+            case "tea":
+                dm.GetTea();
+                return;
+            case "juice":
+                dm.GetJuice();
+                return;
+            default:
+                System.Console.WriteLine("Something went wrong!");
+                return;
+        }
     }
 }
