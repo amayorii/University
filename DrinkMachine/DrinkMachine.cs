ccbd51dd75a66cdbd1658031e1b7bd7858d709d3
Nazariy <malashenko.naz@gmail.com>
2024-11-29 17:26:04 +0200

    add TakePayment functionality

--- DrinkMachine/DrinkMachine.cs
+++ DrinkMachine/DrinkMachine.cs
@@ -18,6 +18,25 @@ public class DrinkMachine : IMachineAction
 
     public void TakePayment(int costMoney, int costPoints)
     {
+        string pathMoney = "./Payment/money.txt", pathBonus = "./Payment/bonus.txt";
 
+        int money = Convert.ToInt32(new StreamReader(pathMoney).ReadLine());
+        int points = Convert.ToInt32(new StreamReader(pathBonus).ReadLine());
+
+        if (money < costMoney && points < costPoints) throw new NoMoneyException("You don't have money 😔");
+
+        if (points >= costPoints)
+        {
+            System.Console.WriteLine("You have enough points, do you want to spend them? (y/n)");
+            if (Console.ReadLine() == "y")
+            {
+                File.WriteAllText(pathBonus, (points -= costPoints).ToString());
+                System.Console.WriteLine($"You spent {costPoints} points\nNow your balance is: ${money} | {points} pts");
+                return;
+            }
+        }
+        File.WriteAllText(pathMoney, (money -= costMoney).ToString());
+        File.WriteAllText(pathBonus, (++points).ToString());
+        System.Console.WriteLine($"You spent ${costMoney} and gain 1 point\nNow your balance is: ${money} | {points} pts");
     }
 }
