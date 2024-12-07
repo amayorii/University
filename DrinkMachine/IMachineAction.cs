43cf31545a0c8761519f250c026c26e555eb2104
Nazariy <malashenko.naz@gmail.com>
2024-12-07 13:05:42 +0200

    replace getdrink methods with one, add givePoints to TakePayment

--- DrinkMachine/IMachineAction.cs
+++ DrinkMachine/IMachineAction.cs
@@ -1,8 +1,6 @@
 namespace DrinkMachine;
 interface IMachineAction
 {
-    void GetTea();
-    void GetCoffee();
-    void GetJuice();
-    void TakePayment(int costMoney, int costPoints);
+    void Get(IDrink drink);
+    void TakePayment(int costMoney, int costPoints, int givePoins);
 }
